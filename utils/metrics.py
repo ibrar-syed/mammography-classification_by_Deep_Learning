8eb6779e41d07ebf143a3036acfbc390ce78f20d
Syed Ibrar Hussain <syedhussain.ibrar@gmail.com>
2025-08-04 12:57:48 +0200

    metrics.py

---
+++ utils/metrics.py
@@ -0,0 +1,12 @@
+# utils/metrics.py
+
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, cohen_kappa_score
+
+def evaluate_classification(y_true, y_pred):
+    return {
+        "accuracy": round(accuracy_score(y_true, y_pred), 4),
+        "precision": round(precision_score(y_true, y_pred, average='weighted'), 4),
+        "recall": round(recall_score(y_true, y_pred, average='weighted'), 4),
+        "f1_score": round(f1_score(y_true, y_pred, average='weighted'), 4),
+        "cohen_kappa": round(cohen_kappa_score(y_true, y_pred), 4),
+    }
