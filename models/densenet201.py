3b0665f2e6dd3a747b538697308ecc02ed6f8d03
Syed Ibrar Hussain <syedhussain.ibrar@gmail.com>
2025-08-04 12:53:00 +0200

    densenet201.py

---
+++ models/densenet201.py
@@ -0,0 +1,27 @@
+# models/densenet201.py
+
+from tensorflow.keras.applications import DenseNet201
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Flatten, BatchNormalization, Dense, Activation, Dropout
+from tensorflow.keras.optimizers import Adam
+
+def build_densenet201(input_shape=(299, 299, 3), num_classes=3):
+    base_model = DenseNet201(input_shape=input_shape, include_top=False, weights='imagenet')
+    for layer in base_model.layers:
+        layer.trainable = False
+
+    model = Sequential([
+        base_model,
+        Flatten(),
+        BatchNormalization(),
+        Dense(1024, kernel_initializer='he_uniform'),
+        BatchNormalization(),
+        Dense(512, kernel_initializer='he_uniform'),
+        BatchNormalization(),
+        Activation('relu'),
+        Dropout(0.4),
+        Dense(num_classes, activation='softmax')
+    ])
+
+    model.compile(optimizer=Adam(), loss='categorical_crossentropy', metrics=['accuracy'])
+    return model
